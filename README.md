4fea44c7413356ddbe40725349d006a44378c615
Amit Zur <amitzur@Caprizas-MacBook-Air.local>
2016-02-04 12:57:03 +0200

    initial webpack implementation

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # react-etc-boilerplate
-Yet another, and yet my very own personal boilerplate for react-webpack-etc 
+Yet another, and yet my very own personal boilerplate for react-webpack-etc
+
+The intention is to progress this boilerplate at the same pace of my learning curve. Each tag should be a complete usable project. The roadmap as I see it:
+- webpack
+- react
+- react-router
+- flux (probably redux)
+
+## Webpack
+A great tutorial is at (http://www.pro-react.com/materials/appendixA/)[http://www.pro-react.com/materials/appendixA/]
+Naturally this should begin with the css loaders, source maps, HMR, ES6, and npm scripts.
+Next there should be a production configuration.
+
+## React
+Adding react babel tranformation, react HMR, initial JSX file and components directory.
+
+## React Router
+Not sure how this works
+
+## Redux
+Adding redux, redux-react, redux-simple-router plus some middlewares. Directory structure will change considerably.
+
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+var webpack = require('webpack');
+
+module.exports = {
+    devtool: 'source-map',
+    entry: {
+        bundle: [
+            './src/index.js'
+        ]
+    },
+    module: {
+        loaders: [
+            { test: /\.js$/, loaders: ['babel'], exclude: /node_modules/ },
+            { test: /\.css$/,  loader: "style!css" },
+            { test: /\.scss/,  loaders: ["style", "css", "sass"] },
+            { test: /\.woff(2)?(\?.+)?$/, loader: "url-loader?limit=10000&minetype=application/font-woff" },
+            { test: /\.(ttf|eot|svg)(\?.+)?$/, loader: "file-loader" }
+        ]
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    output: {
+        path: 'dist',
+        publicPath: '/',
+        filename: '[name].js'
+    },
+    plugins: [
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin(),
+        new HtmlWebpackPlugin()
+    ]
+};
